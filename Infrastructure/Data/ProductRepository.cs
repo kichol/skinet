f67df65873569ba41e462ea9b94ab492da830ab5
kichol <krzysztof.ligeska@gmail.com>
2020-10-04 20:33:18 +0200

    End of section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,5 +1,6 @@
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -17,6 +18,7 @@ namespace Infrastructure.Data
         }
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+
             return await _context.Products
             .Include(p => p.ProductType)
             .Include(p => p.ProductBrand)
